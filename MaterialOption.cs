c9d4a66c53f81ff7b10f2e2270dd0bd800a66633
Endlich123 <mar.pas.456@gmail.com>
2026-06-25 09:21:19 +0200

    Delete MaterialOption.cs

--- MaterialOption.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SkyrimCraftingTool
-{
-    public class MaterialOption
-    {
-        public string KeyString { get; set; }   // FormKey as string
-        public string DisplayName { get; set; } // EditorID
-    }
-
-}
