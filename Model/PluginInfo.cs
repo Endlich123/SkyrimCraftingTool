8a78e02149ccfd6cfad69591a983bd028926ec54
Endlich123 <mar.pas.456@gmail.com>
2026-07-09 11:29:51 +0200

    Delete Model directory

--- Model/PluginInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SkyrimCraftingTool.Model
-{
-    public class PluginInfo
-    {
-        public string FileName { get; set; }
-        public List<string> FullPaths { get; set; } = new();
-    }
-}
